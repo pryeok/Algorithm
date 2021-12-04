8dd383ecea1c2d4c1d20e3c288a1f530f639ce78
Park Ryuck <pryeok@gmail.com>
2021-12-04 21:00:36 +0900

    algorithm

---
+++ app/src/main/java/com/eomcs/algorithm/TimeComplexity.java
@@ -0,0 +1,15 @@
+package com.eomcs.algorithm;
+
+public class TimeComplexity {
+
+  public static void main(String[] args) {
+
+    int sum = 0;
+    int N = 5;
+    sum = N*(N+1)/2;
+
+    System.out.println(sum);
+
+  }
+
+}
---
+++ app/src/main/java/com/eomcs/algorithm/stack.java
@@ -0,0 +1,11 @@
+package com.eomcs.algorithm;
+
+public class stack {
+
+  public static void main(String[] args) {
+
+
+
+  }
+
+}
